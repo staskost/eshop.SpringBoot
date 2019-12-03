678b5c627016b8a1d76463efa48aee865b8e27c2
Konstantinos Stasinos <Konstantinos.Stasinos@gr.ey.com>
2019-12-03 17:23:16 +0200

    Small changes

--- src/main/java/com/staskost/eshop/repos/ProductRepository.java
+++ src/main/java/com/staskost/eshop/repos/ProductRepository.java
@@ -8,20 +8,20 @@ import org.springframework.stereotype.Repository;
 import com.staskost.eshop.model.Product;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer>{
-	
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
 	Product findByName(String name);
-	
+
 	List<Product> findByPrice(Double price);
-	
+
 	List<Product> findByPriceBetween(double priceMin, double priceMax);
-	
+
 	List<Product> findByNameContainingIgnoreCase(String name);
-	
+
 	List<Product> findByNameStartsWithIgnoreCase(String name);
-	
+
 	List<Product> findByCategory(String category);
-	
+
 	int countByCategory(String category);
 
 }
